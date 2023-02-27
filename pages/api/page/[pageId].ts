e87f1c95e53c80101eaffbe60e41fae26bfea126
Tino Muzambi <MZMTIN002@myuct.ac.za>
2023-02-27 12:13:51 +0200

    Adds page retrieval.

--- pages/api/page/[pageId].ts
+++ pages/api/page/[pageId].ts
@@ -1,7 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Client } from "@notionhq/client";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-	res.status(200).json({ message: "Hello there" });
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	const notion = new Client({
+		auth: process.env.NOTION_INTEGRATION_TOKEN,
+	});
+
+	console.log(`New request - ID:${req.query.pageId}`);
+
+	const page = await notion.pages.retrieve({
+		page_id: req.query.pageId as string,
+	});
+
+	res.status(200).json({ data: page });
 };
 
 export default handler;
